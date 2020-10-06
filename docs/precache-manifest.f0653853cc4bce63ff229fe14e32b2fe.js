c23718b4ffd08506b5e824d549735600efe2badc
Jose Baptista <jbaptista.dev@gmail.com>
2020-10-05 21:04:08 -0400

    rename folder build for github pages

---
+++ docs/precache-manifest.f0653853cc4bce63ff229fe14e32b2fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d938a0e73ebc03640047c170fc474f00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a11c1610c1365ddf590a",
+    "url": "/static/css/main.bcc2736b.chunk.css"
+  },
+  {
+    "revision": "8ff68b5a58963c010348",
+    "url": "/static/js/2.8703cab2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8703cab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a11c1610c1365ddf590a",
+    "url": "/static/js/main.4b76e611.chunk.js"
+  },
+  {
+    "revision": "a70e4ae6bdd791779146",
+    "url": "/static/js/runtime-main.1e57df78.js"
+  }
+]);
